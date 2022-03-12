58fe78ccb97497949cc2e4c77bbbe31cdaa13046
Samigirum <samiabge23@gmail.com>
2022-03-12 18:07:16 +0000

    Printalphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,21 +8,21 @@ int main(void)
 
 {
 char lower_case;
-char upper_case;
+char upper_case:
 
-for (lower_case = 'a'; lower_case <= 'Z' ; lower_case++)
+for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 {
 putchar(lower_case);
 }
 
 
 for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
-
+{
 putchar(upper_case);
 }
 
 
-putchar ('\n');
+putchar (0);
 
 return (0);
 
