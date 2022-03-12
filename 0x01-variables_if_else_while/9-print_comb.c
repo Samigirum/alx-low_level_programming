849d13c20dfaccdfe3efad7fe39b86632a0efcbd
Samigirum <samiabge23@gmail.com>
2022-03-12 23:53:42 +0300

    my last commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
-* 
 * main - Entry point
+* 
 * Return: Always 0 (success/correct)
 */
 int main(void)
 
 {
 int number;
-  
 for (number = 48; number <= 57; number++)
 {
 putchar(number);
