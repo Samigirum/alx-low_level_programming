03f8ec70afab9b8cc74300ab7a57733f8cee5cc8
Samigirum <samiabge23@gmail.com>
2022-03-12 23:55:28 +0300

    my last commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
-* main - Entry point
-* 
+* main - Entry point 
 * Return: Always 0 (success/correct)
 */
 int main(void)
@@ -24,5 +23,4 @@ putchar (' ');
 putchar ('\n');
 
   return (0);
-  
 }
