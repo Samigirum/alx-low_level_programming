e4be392ae585fbcb6eb93d48a21910ad6b2054e7
Samigirum <samiabge23@gmail.com>
2022-03-15 21:17:06 +0300

    putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,15 +5,13 @@
 * description: it print the word_putchar
 * Return: 0
 */
-int main(void)
 
-{
 char word [9] = "_putchar\n";
 int i;
 
 for (i =  0; i < 9; i++)
 {
-putchar(word[i]);
+_putchar(word[i]);
 }
 
 return (write(0, i, 8);
