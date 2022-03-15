42e22edc0bdfbe7d98ad86140d0964ca46bbfdb3
Samigirum <samiabge23@gmail.com>
2022-03-15 11:12:11 +0000

    My first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stidio.h>
+/**
+
+* main - Entry point
+*
+* Return: Always 0 (Success/correct)
+*/
+int main(void)
+{
+
+char ch = '_putchar';
+
+putchar(ch);
+
+putchar('\n');
+
+return (0);
+
