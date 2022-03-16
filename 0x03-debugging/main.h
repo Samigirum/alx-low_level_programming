40c3dc959ba2f81cbc2d646ec65f8ae5ec9d042f
Samigirum <samiabge23@gmail.com>
2022-03-16 19:44:46 +0000

    my last commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,6 +6,7 @@
 void positive_or_negative(int n);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
+void print_renaining_days(int month, int day, int year);
 #endif
 
 
