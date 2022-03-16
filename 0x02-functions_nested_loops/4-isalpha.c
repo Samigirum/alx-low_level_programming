3368aaff8d56efecd88ca4e384f40ceccffc4dbf
Samigirum <samiabge23@gmail.com>
2022-03-16 07:01:07 +0300

    print isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,14 +3,14 @@
 /**
 *
 * _isalpha - Return 1 if c is letter. lowecase or uppercase
-*@c: the int to print
+* @c: the int to print
 * Return: Always 0.
 */
 int _isalpha(int c)
 
 {
 
-if (( c > 'a' && c < 'z') || ( c > 'A' && c < 'Z'))
+if ( ( c > 'a' && c < 'z') || ( c > 'A' && c < 'Z'))
 {
 return (1);
 }
