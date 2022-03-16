fc741ce3c9e93348d4da96dfc232d08ff6d3324d
Samigirum <samiabge23@gmail.com>
2022-03-16 04:35:02 +0000

    print isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,11 +6,12 @@
 * @c: the int to print
 * Return: Always 0.
 */
+
 int _isalpha(int c)
 
 {
 
-if ( ( c > 'a' && c < 'z') || ( c > 'A' && c < 'Z'))
+if ((c > 'a' && c < 'z') || ( c > 'A' && c < 'Z'))
 {
 return (1);
 }
