333e8698651cb3e25b680e54699d11dafd88e554
Samigirum <samiabge23@gmail.com>
2022-03-16 07:20:32 +0300

    print sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-* print_sign - Entry point
-* Description: return 0 letter not lowercase, 1 letter lowercase
-*@n: the int to print
-* Retrun: Always 0.
-*/
+ print_sign - return 0 letter not lowercase, 1 letter lowercase
+ *
+ *@n: the int to print
+ * Return: Always 0.
+ */
 int print_sign(int n)
 {
 if (n > 0)
