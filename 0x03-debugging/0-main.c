72a599cd2b40c490c53c6472ffc64f37c96c0cd6
alx-pre_course <samiabge23@gmail.com>
2022-03-17 10:37:46 +0300

    main_b.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * main - main
 * Return: 0
@@ -7,11 +6,9 @@
 
 int main(void)
 {
-int n;
-n = 0;
-  
-positive_or_negative(n);
+int i;
+i = 0;
+
+positive_or_negative(i);
 
 return (0);
-
-}
