09448aa2e8aa1e08256c2e1e6e6507988da2b993
Samigirum <samiabge23@gmail.com>
2022-03-17 19:19:02 +0000

    2nd commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,20 @@
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_training(int size);
+#inside "main.h"
+/**
+* _isdigit - if is a digit
+*@c: print int
+* Return: Always 0.
+*/
+
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
