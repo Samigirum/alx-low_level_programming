b265aa27ad51a34fcbe881a026c4a20e36e1a9df
Samigirum <samiabge23@gmail.com>
2022-03-21 18:13:33 +0000

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* print_rev - print char
+*@s: print char
+*
+* Return: Always 0.
+*/
+
+void print_rev(char *s)
+{
+int a;
+
+a = 0;
+while (s[a] != '\0')
+{
+a++;
+}
+
+for (i = i -1 ; i >=0; i--)
+}
+_putchar (s[i]);
+}
+
+_putchar ('\n');
+
+}
