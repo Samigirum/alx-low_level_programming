d12eae2857b1de2aba793a2183654ee1cf3fede7
Samigirum <samiabge23@gmail.com>
2022-03-21 15:25:17 +0000

    second

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+* swap_int - to swap tow integer
+*@a: print int
+*@b: print int
+* Return: Always 0.
+*/
+void swap_int(int *a, int *b)
+{
+int iTemp;
+iTemp = *a;
+*a = *b;
+*b = itemp;
