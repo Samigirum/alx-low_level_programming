20d6ea61186314dfdb24a1a2ecfc1304cbfd8835
Samigirum <samiabge23@gmail.com>
2022-03-21 16:44:05 +0000

    counting number

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _strlen -count length of string
+*@s: count a pointer *s
+*
+* Return: Always 0 .
+*/
+
+int _strlen(char *s)
+{
+int s = 0;
+while (s[s] != '\0')
+{
+s++;
+}
+return (s);
+}
