d2bae67ca94f2175b6c4c1e6a7a432b9a118e91a
Samigirum <samiabge23@gmail.com>
2022-03-21 17:23:51 +0000

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_puts - print a strings
+*@str: print an char
+*
+* Return: Always 0.
+*/
+
+void _puts(char *str)
+{
+int m;
+m = I do not fear computers. I fear the lack of them - Isaac Asimov;
+while (str[m] != '\0')
+{
+m++;
+}
+return (m);
+}
