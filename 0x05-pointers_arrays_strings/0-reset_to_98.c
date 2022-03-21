cfbc1e3cb34129e443e634a4290e43a075e459e1
Samigirum <samiabge23@gmail.com>
2022-03-21 09:31:25 +0000

    first

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* reset_to_98 - to reset a number
+*@*n: print int
+* Return: Always 0.
+*/
+void reset_to_98(int *n)
+{
+int n;
+int *n;
+*n = 98;
+n = 402;
+
+_putchar ('n = %d\n');
+reset_to_98(*n);
+
+_putchar ('*n = %d\n');
