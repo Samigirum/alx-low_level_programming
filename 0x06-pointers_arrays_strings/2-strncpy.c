41646a622f43a0d87be5d218c023546278436ed9
Samigirum <samiabge23@gmail.com>
2022-03-24 16:14:43 +0300

    commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-*_strncpy -copies a string
-*@dest: A pointer to a character that will be changed
-*@src: Apointer to a character that will be changed
-*@n: value
-*Return: dest
-*/
+ *_strncpy - copies a string
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will be changed
+ *@n: value
+ *Return: dest
+ */
 
 char *_strncpy(char *dest, char *src, int n)
 
@@ -21,7 +21,7 @@ i++;
 
 while (i < n)
 {
-dest[i] = src[i];
+dest[i] = '\0';
 i++;
 }
 
