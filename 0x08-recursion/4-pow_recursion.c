ffa895190f91c934e9a1e3127a9ec764991e600b
Samigirum <samiabge23@gmail.com>
2022-03-30 15:42:17 +0000

    fifth

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*_pow_recursion - return the value of x power y
+*@x: integer
+*@y: integer
+*Return: pow function
+*/
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+
+else if (y != 0)
+return(x * _pow_recursion(x, y - 1));
+
+else
+return (1)
+}
+
