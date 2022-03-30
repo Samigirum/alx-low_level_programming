50ed9418b7f924e3ba66e9d21b48fed3aece8f39
Samigirum <samiabge23@gmail.com>
2022-03-30 19:37:18 +0300

    seventh

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,50 +1,57 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - size
- * @s: pointer to string params
- * Return: recursion
+ *evaluate_num - recursion loop
+ *@num: num
+ *@iterator: number to iterate
+ *Return: return 1 or 0
  */
 
-int _strlen_recursion(char *s)
+int evaluate_num(int num, int iterator)
 {
-	if (!*s)
-	{
-		return (0);
-	}
-	return (1 + _strlen_recursion(++s));
+
+if (iterator == num - 1)
+{
+return (1);
+}
+
+else if (num % iterator == 0)
+{
+return (0);
+}
+
+if (num % iterator != 0)
+{
+return (evaluate_num(num, iterator + 1));
+}
+
+return (0);
+
 }
 
 /**
- * p1 - palindrome
- * @s: pointer to string
- * @l: position
- * Return: boolena
+ *is_prime_number - evaluate prime or not
+ *@num: number
+ *Return: return 1 prime - return 0 otherwise
  */
 
-int p1(char *s, int l)
+int is_prime_number(int num)
 {
-	if (l < 1)
-	{
-		return (1);
-	}
 
-	if (*s == *(s + l))
-	{
-		return (p1(s + 1, l - 2));
-	}
-	return (0);
+int iterator;
+
+iterator = 2;
+
+/* only greater than 2*/
+if (num < 2)
+{
+return (0);
 }
 
-/**
- * is_palindrome - palindrome
- * @s: pointer to string
- * Return: recursion
- */
-
-int is_palindrome(char *s)
+if (num == 2)
 {
-	int len = _strlen_recursion(s);
+return (1);
+}
 
-	return (p1(s, len - 1));
+return (evaluate_num(num, iterator));
 }
